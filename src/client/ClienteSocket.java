f66639e8ab76185278da69053aa2cde21fe53769
anagallardo0082 <anagallardo.0082@gmail.com>
2026-03-23 21:28:53 -0600

    fisrt

---
+++ src/client/ClienteSocket.java
@@ -0,0 +1,16 @@
+package client;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ClienteSocket {
+	
+	public static void main(String[] args) throws IOException {
+		
+		
+		
+		
+	}
+
+}
---
+++ src/client/TCPClient.java
@@ -0,0 +1,51 @@
+package client;
+
+import java.io.*; 
+import java.net.Socket; 
+import java.util.Scanner; 
+import java.io.IOException;
+
+public class TCPClient {
+	
+	   public TCPClient() throws IOException { 
+
+	        // Conectarse al servidor en localhost en el puerto 2020 
+		    final int puerto = 2022;
+	        Socket  socket  =  new  Socket ( "localhost" , puerto ); 
+	        System.out.println( "Conectado correctamente al servidor." ); 
+
+	        // Configurar flujos de entrada y salida para la comunicación 
+	        BufferedReader  in_socket  =  new  BufferedReader ( new  InputStreamReader (socket.getInputStream())); 
+	        PrintWriter  out_socket  =  new  PrintWriter ( new  OutputStreamWriter (socket.getOutputStream()), true ); 
+	        
+
+	        // Recibir y mostrar el mensaje de bienvenida del servidor 
+	        String  message  = in_socket.readLine(); 
+	        System.out.println( "El servidor dice: " + message); 
+
+	        // Enviar un mensaje al servidor
+	        while(!message.equals("close")){
+	        	Scanner  keyboard  =  new  Scanner (System.in); 
+		        message = keyboard.nextLine(); 
+		        out_socket.println(message); 
+		        
+		        System.out.println("hOLA: " +message);
+		        
+		        String  message2  = in_socket.readLine(); 
+		        System.out.println( "Accion Server: " + message2); 
+		    }
+	        // Cerrar la conexión
+	        socket.close(); 
+	        System.out.println( "El puerto 2020 se cerró correctamente." ); 
+	    } 
+
+	    public  static  void  main (String[] args) { 
+
+	        try { 
+	            new  TCPClient (); // Iniciar el cliente
+
+	         } catch (Exception e) { 
+	            e.printStackTrace(); 
+	        } 
+	    } 
+}
